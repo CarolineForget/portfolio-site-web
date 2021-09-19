5d897fef14f35f5fe1a38c30c010a7075514ce6b
Caroline Forget <caro.forget23@gmail.com>
2021-09-19 17:13:44 -0400

    Styles Css pour le carousel de la page A propos. Installation de la librairie Swiper.js

--- src/scripts/components/Carousel.js
+++ src/scripts/components/Carousel.js
@@ -13,10 +13,9 @@ export default class Carousel {
     this.defaultOptions = {
       slidesPerView: 1,
       spaceBetween: 20,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
     };
 
